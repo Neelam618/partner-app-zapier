90d37824d1c3699363ed26feb388beec41e2a454
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-29 17:22:37 +0530

    cta added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,17 +2,18 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
+        <div id="home">
             <div className="cta">
                 <div className="ctaLeftCol">
-                    <div className="ctaTitle">Build a Zapier Integration</div>
+                    <h3 className="ctaTitle">Build a Zapier Integration</h3>
                     <p>Zapier’s Free Developer Platform connects your API to the web’s top apps. Learn more in our Quick Start Guide.</p>
                 </div>
                 <div className="ctaRightCol">
-                    <button>Start a Zapier Integration</button>
+                    <button className="startBtn">Start a Zapier Integration</button>
                     <p>Or start your integration with the CLI</p>
                 </div>
             </div>
+            <h1>Integrations</h1>
         </div>
     )
 }
