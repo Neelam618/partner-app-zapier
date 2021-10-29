ead5a5b68ae064b2e1b1384c74bd9759c738f061
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-29 15:44:36 +0530

    renamed components and added home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="cta">
+                <div className="ctaLeftCol">
+                    <div className="ctaTitle">Build a Zapier Integration</div>
+                    <p>Zapier’s Free Developer Platform connects your API to the web’s top apps. Learn more in our Quick Start Guide.</p>
+                </div>
+                <div className="ctaRightCol">
+                    <button>Start a Zapier Integration</button>
+                    <p>Or start your integration with the CLI</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
